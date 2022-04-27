4b17def244647402fa9f61f4244074f39ac4d67e
MicaelD <micaeldiniz6@gmail.com>
2022-04-27 19:49:19 -0300

    Feat: setting up the basic client routes

---
+++ app/routes/client_route.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+from app.controllers import clients_controller
+
+bp = Blueprint("clients",__name__,url_prefix="/clients")
+
+bp.get("")(clients_controller.retrieve_clients)
+bp.post("")(clients_controller.create_client)
+bp.get("/<int:client_id>")(clients_controller.client_by_id)
+bp.patch("/<int:client_id>")(clients_controller.update_client)
+bp.delete("/<int:client_id>")(clients_controller.delete_client)
