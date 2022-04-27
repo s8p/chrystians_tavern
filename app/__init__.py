f482ceb084fb58a7566f5b79894a125ce7371d1c
Marcos <marcoveio.bpp@gmail.com>
2022-04-27 15:17:24 -0300

    create boxes routes structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from app import routes
 from app.configs import database, migration
 
 
@@ -8,5 +8,6 @@ def create_app():
 
     database.init_app(app)
     migration.init_app(app)
+    routes.init_app(app)
 
     return app
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
+from app.routes.boxes_routes import bp as bp_box
 
 
 bp_api = Blueprint("api", __name__)
 
 
 def init_app(app):
+
+    bp_api.register_blueprint(bp_box)
 
     app.register_blueprint(bp_api)
