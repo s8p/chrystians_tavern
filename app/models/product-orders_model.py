31d2b8e6f20ee0a4cf16cee4b650ff5941822fd2
Rodrigo Souza <souzabpr@tuta.io>
2022-04-27 19:45:29 +0000

    style: auto lint with black

--- app/models/product-orders_model.py
+++ app/models/product-orders_model.py
@@ -6,22 +6,20 @@ from dataclasses import dataclass
 from app.models.order_model import OrderModel
 from app.models.products_model import ProductModel
 
+
 @dataclass
 class ProductOrderModel(db.Model):
 
-    id : int
-    product_id : ProductModel
-    order_id : OrderModel
-    amount : int
+    id: int
+    product_id: ProductModel
+    order_id: OrderModel
+    amount: int
 
     __tablename__ = "product_orders"
-
 
     id = Column(Integer, primary_key=True)
     product_id = Column(Integer, ForeignKey("products.id"))
     order_id = Column(Integer, ForeignKey("orders.id"))
-    amount = Column(Integer,default=1)
+    amount = Column(Integer, default=1)
     productId = relationship("ProdutModel", backref=backref("product", uselist=False))
-    orderId= relationship("OrderModel",backref=backref("order", uselist=False))
-
-
+    orderId = relationship("OrderModel", backref=backref("order", uselist=False))
