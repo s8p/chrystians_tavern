1260171ff0818fda2436ebd62572162128993b68
Rodrigo Souza <souzabpr@tuta.io>
2022-04-27 22:03:12 +0000

    style: auto lint with black

--- app/routes/product_route.py
+++ app/routes/product_route.py
@@ -2,10 +2,10 @@ from flask import Blueprint
 
 from app.controllers import products_controller
 
-bp = Blueprint('products', __name__, url_prefix='products')
+bp = Blueprint("products", __name__, url_prefix="products")
 
-bp.post('')(products_controller.create_product)
-bp.get('')(products_controller.retrieve_products)
-bp.get('/<int:product_id>')(products_controller.product_by_id)
-bp.patch('/<int:product_id>')(products_controller.update_product)
-bp.patch('/<int:product_id>')(products_controller.delete_product)
+bp.post("")(products_controller.create_product)
+bp.get("")(products_controller.retrieve_products)
+bp.get("/<int:product_id>")(products_controller.product_by_id)
+bp.patch("/<int:product_id>")(products_controller.update_product)
+bp.patch("/<int:product_id>")(products_controller.delete_product)
