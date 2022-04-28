ad27dd9ca832f690fc70acdf839725bcd0404244
Rodrigo de Souza <souzabpr@tuta.io>
2022-04-28 11:42:06 -0300

    feat: register product and client routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
-
+from product_route import bp as bp_product
+from client_route import bp as bp_client
 
 bp_api = Blueprint("api", __name__)
 
 
 def init_app(app):
-
+    bp_api.register_blueprint(bp_client)
+    bp_api.register_blueprint(bp_product)
     app.register_blueprint(bp_api)
