ac00686298565d06a918f2c1543b45e7104a66b6
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-04-28 17:37:26 -0300

    feat: implements migration and versions, and fixes tables incompatibilities

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,5 @@ from .client_orders_model import ClientOrderModel
 from .clients_model import ClientsModel
 from .order_model import OrderModel
 from .products_model import ProductModel
-from .products_order_model import ProductOrderModel
+from .product_orders_model import ProductOrderModel
+ 
