9306e30955a59cd50fdc9e85ed12e56a87869ae6
Rodrigo de Souza <souzabpr@tuta.io>
2022-04-28 15:39:34 -0300

    feat: initial migration setup

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .boxes_model import BoxesModel
+from .categories_model import CategoriesModel
+from .client_orders_model import ClientOrderModel
+from .clients_model import ClientsModel
+from .order_model import OrderModel
+from .products_model import ProductModel
+from .products_order_model import ProductOrderModel
