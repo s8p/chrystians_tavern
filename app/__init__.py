70c822cc95bc8de7c97182f9a8c53878a881a2d6
Rodrigo de Souza <souzabpr@tuta.io>
2022-04-28 11:40:59 -0300

    feat: route initialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from app import routes
 
 from app.configs import database, migration
 
@@ -8,5 +9,7 @@ def create_app():
 
     database.init_app(app)
     migration.init_app(app)
+
+    routes.init_app(app)
 
     return app
