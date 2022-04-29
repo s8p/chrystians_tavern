4fb5a1a3c63e781e983f82aa51f9bda9ee4155e5
MicaelD <micaeldiniz6@gmail.com>
2022-04-29 12:45:02 -0300

    Refactor: transform the monthly_price to BRL currency

--- app/services/boxes_services.py
+++ app/services/boxes_services.py
@@ -1,14 +1,20 @@
 import locale
-
 from app.models.boxes_model import BoxesModel
-
-locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
-
 from app.configs.database import db
 from sqlalchemy.orm import Session
 
 
+locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
+
+
+
 def get_all_boxes():
-    session: Session = db.session
-    boxes = session.query(BoxesModel).all()
-    return boxes
+	session: Session = db.session
+	boxes = session.query(BoxesModel).all()
+	adapted_boxes = []
+	for boxe in boxes:
+		adapted_box = dict(**boxe.__dict__)
+		adapted_box.pop("_sa_instance_state")
+		adapted_box.update({"monthly_price":locale.currency(boxe.monthly_price)})
+		adapted_boxes.append(adapted_box)
+	return adapted_boxes
