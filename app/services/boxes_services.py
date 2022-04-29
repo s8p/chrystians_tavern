673a06108e548a05ec39c6944c563fa1f8970a7f
Rodrigo Souza <souzabpr@tuta.io>
2022-04-29 15:04:41 +0000

    style: auto lint with black

--- app/services/boxes_services.py
+++ app/services/boxes_services.py
@@ -7,7 +7,8 @@ locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
 from app.configs.database import db
 from sqlalchemy.orm import Session
 
+
 def get_all_boxes():
-	session: Session = db.session
-	boxes = session.query(BoxesModel).all()
-	return boxes
+    session: Session = db.session
+    boxes = session.query(BoxesModel).all()
+    return boxes
