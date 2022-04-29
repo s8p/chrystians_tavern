ac37ab12c54d9db8716c104ae7dea8d23fb1f3cd
Rodrigo Souza <souzabpr@tuta.io>
2022-04-29 15:52:27 +0000

    style: auto lint with black

--- app/services/boxes_services.py
+++ app/services/boxes_services.py
@@ -7,14 +7,13 @@ from sqlalchemy.orm import Session
 locale.setlocale(locale.LC_MONETARY, "pt_BR.UTF-8")
 
 
-
 def get_all_boxes():
-	session: Session = db.session
-	boxes = session.query(BoxesModel).all()
-	adapted_boxes = []
-	for boxe in boxes:
-		adapted_box = dict(**boxe.__dict__)
-		adapted_box.pop("_sa_instance_state")
-		adapted_box.update({"monthly_price":locale.currency(boxe.monthly_price)})
-		adapted_boxes.append(adapted_box)
-	return adapted_boxes
+    session: Session = db.session
+    boxes = session.query(BoxesModel).all()
+    adapted_boxes = []
+    for boxe in boxes:
+        adapted_box = dict(**boxe.__dict__)
+        adapted_box.pop("_sa_instance_state")
+        adapted_box.update({"monthly_price": locale.currency(boxe.monthly_price)})
+        adapted_boxes.append(adapted_box)
+    return adapted_boxes
