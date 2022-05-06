44f4434b6c724b5ef05ac84d0db6ca3500973967
Rodrigo Souza <souzabpr@tuta.io>
2022-05-06 02:48:10 +0000

    style: auto lint with black

--- app/controllers/orders_controller.py
+++ app/controllers/orders_controller.py
@@ -1,7 +1,7 @@
 from http import HTTPStatus
 from flask import jsonify
 
-from app.configs.database import db 
+from app.configs.database import db
 from app.models import OrderModel, ClientOrderModel, ProductOrderModel, ProductModel
 from app.exceptions.order_exc import ClientNotFound
 
@@ -20,34 +20,37 @@ def get_orders():
 
 def get_order_by_client_id(client_id: int):
     try:
-        session : Session = db.session
+        session: Session = db.session
 
         checking_id(client_id)
 
-        client_orders = session.query(ClientOrderModel).filter_by(client_id= client_id).all()
+        client_orders = (
+            session.query(ClientOrderModel).filter_by(client_id=client_id).all()
+        )
         all_client_orders = []
 
         for order in client_orders:
             products = []
 
-            product_orders = session.query(ProductOrderModel).filter_by(order_id= order.id).all()
+            product_orders = (
+                session.query(ProductOrderModel).filter_by(order_id=order.id).all()
+            )
             each_order = session.query(OrderModel).get(order.id)
 
             for product_order in product_orders:
                 product = session.query(ProductModel).get(product_order.product_id)
                 products.append(product)
-            
+
             single_order = {
-                'id': each_order.id,
-                'date': each_order.date,
-                'price': each_order.price,
-                'products': products
+                "id": each_order.id,
+                "date": each_order.date,
+                "price": each_order.price,
+                "products": products,
             }
 
             all_client_orders.append(single_order)
-        
-        
+
         return jsonify(all_client_orders), HTTPStatus.OK
-    
+
     except ClientNotFound:
-        return {'error': 'Cliente não encontrado!'}, HTTPStatus.NOT_FOUND 
+        return {"error": "Cliente não encontrado!"}, HTTPStatus.NOT_FOUND
--- app/services/orders_services.py
+++ app/services/orders_services.py
@@ -4,6 +4,7 @@ from app.configs.database import db
 from app.exceptions.order_exc import ClientNotFound
 from app.models import ClientsModel
 
+
 def checking_id(id: int):
     session: Session = db.session
 
